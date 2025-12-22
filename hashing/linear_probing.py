7c7b9be2f671fe1a6ced90cd28137147f4f22adc
Mothilal Jadhav <mothilaljadav0304@gmail.com>
2025-12-22 17:23:32 +0530

    implementing linear probing

--- hashing/linear_probing.py
+++ hashing/linear_probing.py
@@ -13,19 +13,27 @@ class Hashmap:
     def hashFun(self,key):
         return key%self.capacity
     
+    def loadFactor(self):
+        return self.size / self.capacity
+
+    
     def insert(self,key,value):
+
+        if self.loadFactor() >= 0.6:
+            self.rehash()
+
 
         hashIndex = self.hashFun(key)
         start = hashIndex
 
         while self.arr[hashIndex] is not None and \
-                self.arr[hashIndex].key is not self.dummy and \
+                self.arr[hashIndex] is not self.dummy and \
                 self.arr[hashIndex].key != key:
             hashIndex = (hashIndex+1)%self.capacity
             if hashIndex == start:
                 return
 
-        if self.arr[hashIndex] is None or self.arr[hashIndex].key is self.dummy:
+        if self.arr[hashIndex] is None or self.arr[hashIndex] is self.dummy:
             self.size += 1
         self.arr[hashIndex] = HashNode(key, value)
 
@@ -58,6 +66,19 @@ class Hashmap:
 
         return -1
     
+    def rehash(self):
+        old_arr = self.arr
+        old_capacity = self.capacity
+
+        self.capacity *= 2
+        self.arr = [None] * self.capacity
+        self.size = 0
+
+        for node in old_arr:
+            if node is not None and node is not self.dummy:
+                self.insert(node.key, node.value)
+
+    
     def sizeCount(self):
         return self.size
     
@@ -66,7 +87,7 @@ class Hashmap:
     
     def display(self):
         for node in self.arr:
-            if node is not None and node.key != -1:
+            if node is not None and node.key is not self.dummy:
                 print(f"{node.key} {node.value}")
 
 
