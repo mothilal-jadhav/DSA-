ddcd037d5ac316067272d5170ade79b10457abae
Mothilal Jadhav <mothilaljadav0304@gmail.com>
2025-12-27 15:31:42 +0530

    print parents of Node

--- trees/tree_implementation.py
+++ trees/tree_implementation.py
@@ -10,3 +10,16 @@ def addChild(parent,child):
     parent.children.append(child)
 
 
+# function to print parents of each node
+
+def printParent(node,parent):
+    if parent is None:
+        print((str(node.data)) + ' -> Null')
+
+    else:
+        print(str(node.data)+ ' -> ' str(parent.data))
+
+    for child in node.children:
+        printParent(child,node)
+
+
